00919bb210dccee87f2af56305ba14d41aa8ce74
et56 <595914343@qq.com>
2018-09-05 16:00:04 +0800

    add autocomplete, colorpicker, datetimepicker, pagination, progress, tag, etc.

---
+++ demo/vue/src/views/beanui/demo.view.LoginComponent.vue
@@ -0,0 +1,42 @@
+
+
+<template>
+<div class="app-container">
+    <el-form ref="loginForm" :model="loginForm" label-width="80px">
+<el-form-item
+label="username"
+>
+<el-input v-model="loginForm.username" auto-complete="on" prefix-icon="el-icon-message" >
+</el-input>
+</el-form-item>
+<el-form-item
+label="password"
+>
+<el-input v-model="loginForm.password" type="password" auto-complete="on" prefix-icon="el-icon-goods" suffix-icon="el-icon-more" >
+</el-input>
+</el-form-item>
+<el-form-item
+>
+<el-button
+    size="medium"
+    text="login"
+>
+    login
+</el-button>
+</el-form-item>
+    </el-form>
+</div>
+</template>
+<script>
+import request from '@/utils/request'
+
+  export default {
+    data() {
+      return {
+                loginForm: {"password":"","loginButoon":"","username":""}
+      }
+    },
+    methods: {
+    }
+  }
+</script>
